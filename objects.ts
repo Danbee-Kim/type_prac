38875e06d3eb9e00ed45a6a0ce78f27cc78bde87
Danbee Kim <kdb2360@gmail.com>
2022-11-03 02:01:24 +0900

    객체 타입

--- objects.ts
+++ objects.ts
@@ -4,9 +4,12 @@ function printName(person: { first: string; last: string }): void {
 }
 
 printName({ first: "Thomas", last: "Jenkins" });
+// printName({ first: "Thomas",last: "Jenkins", age: 40 }); => error
+// 객체 리터럴은 알려진 프로퍼팀나 지정할 수 있음
 
 const singer = { first: "Mick", last: "Jagger", age: 473, isAlive: true };
 printName(singer);
+//사전에 객체를 변수로 가지고 있으면 지정된 프로퍼티 외에는 무시되면서 오류가 발생하지 않음
 
 // let coordinate: { x: number; y: number } = { x: 34, y: 2 };
 
@@ -21,10 +24,11 @@ printName(singer);
 //   return { x: point.x * 2, y: point.y * 2 };
 // }
 
-// type Point = {
-//   x: number;
-//   y: number;
-// };
+//Type Alias - 타입 별칭
+type Point = {
+  x: number;
+  y: number;
+};
 
 let coordinate: Point = { x: 34, y: 2 };
 
@@ -68,11 +72,12 @@ printSong(mySong);
 type Point = {
   x: number;
   y: number;
-  z?: number;
+  z?: number;   //optional인 경우 "?"" 추가
 };
 
 const myPoint: Point = { x: 1, y: 3 };
 
+//readonly 제어자
 type User = {
   readonly id: number;
   username: string;
@@ -94,7 +99,7 @@ type Colorful = {
   color: string;
 };
 
-type ColorfulCircle = Circle & Colorful;
+type ColorfulCircle = Circle & Colorful;     //교차 타입 
 
 const happyFace: ColorfulCircle = {
   radius: 4,
@@ -110,7 +115,7 @@ type Dog = {
 
 type CatDog = Cat &
   Dog & {
-    age: number;
+    age: number;      //교차 타입에 age 추가
   };
 
 const christy: CatDog = {
