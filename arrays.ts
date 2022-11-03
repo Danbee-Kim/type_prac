3ff849b743178b3eafda9ed4aff2558f33d3eb53
Danbee Kim <kdb2360@gmail.com>
2022-11-03 16:06:11 +0900

    배열 타입

---
+++ arrays.ts
@@ -0,0 +1,10 @@
+//Using Brackets:
+  //string array
+  let names: string[]= ["hello", "world"];
+  //number array
+  let ages: number[]=[24,32,19,29];
+//An alternate syntax
+  //string array
+  let names: Array<string>=["hello","world"];
+  //number aaray
+  let ages: Array<number>=[24,32,19,29];
